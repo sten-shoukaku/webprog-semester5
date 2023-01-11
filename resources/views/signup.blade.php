f7c395004a8477ecad2fc87e6d23993cf053245f
DenzelCIS <95424009+DenzelCIS@users.noreply.github.com>
2023-01-11 14:31:08 +0700

    Update signup.blade.php

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -44,14 +44,6 @@
                         </div>
                     </div>
                 </div>
-                {{-- <p class="fw-lighter" id="welcome-text">WELCOME TO</p>
-                <p class="pt-4" id="welcome-logo">O N T I C K</p>
-                <p class="fw-lighter pt-1" id="welcome-desc">< Your Ticket Is Yours, So Here's Our List ></p>
-                <div class="" id="welcome-btn">
-                    <a href="/signin" class="btn border-light" id="welcome-btn-detail">
-                        <p class="px-4">GET STARTED</p>
-                    </a>
-                </div> --}}
             </div>
         </div>
     </div>
