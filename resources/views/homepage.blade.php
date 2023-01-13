ef1a8dd0477a131a0d92ae511ba7604997841e0e
shoukaku <shoukaku@duck.com>
2023-01-13 15:33:09 +0700

    display logo, sign out navbar button breaks

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,15 +1,23 @@
 @extends('layout.master_member')
 
+<link rel="stylesheet" href="{{URL::asset('css/homepage.css')}}">
+
 @section('title', 'Home')
 
 @section('content')
 
 <div
-    class="d-flex flex-wrap justify-content-center">
+    class="d-flex flex-wrap justify-content-center align-items-center"
+    id="home-banner">
     <img
         src="./images/homepage_banner.jpg"
         alt=""
         style="width: 100vw">
+    <p
+        class="position-absolute"
+        id="ontick-logo">
+        O N T I C K
+    </p>
 </div>
 
 @endsection
