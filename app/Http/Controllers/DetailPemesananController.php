16024ccdb1b4d80814f64b607d24c5afc425015b
Chertine <90467809+Chertine@users.noreply.github.com>
2023-01-20 04:32:52 +0700

    pemesanan and order page

---
+++ app/Http/Controllers/DetailPemesananController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DetailPemesananController extends Controller
+{
+    //
+    public function index() {
+        return view('detailpemesanan');
+    }
+}
