4ea8677573ebf5041ac3cb8fe7203a03c5935657
Chertine <90467809+Chertine@users.noreply.github.com>
2023-01-25 12:15:50 +0700

    Update vieworder view

--- resources/views/vieworder.blade.php
+++ resources/views/vieworder.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="d-flex align-items-center justify-content-center" style="width: 100vw;">
     <div class="d-flex flex-wrap align-items-center justify-content-center" style="margin: 3rem 2rem;">
-        <div class="d-flex flex-wrap flex-column bg-white" style="margin: 5rem 1rem;  padding: 0.2rem 2rem 3rem 2rem; height: 37rem; width: 70rem">
+        <div class="d-flex flex-wrap flex-column bg-white" style="margin: 5rem 1rem;  padding: 0.2rem 20rem 3rem 2rem; height: 37rem">
             <div class="section-title">
                 <h3>Riwayat Pemesanan</h3>
             </div>
@@ -25,19 +25,17 @@
                         </div>
                     </div>
                     <div class="d-flex flex-row justify-content-between" style="max-width: 35rem; border-bottom: 0.1rem solid; padding-bottom: 1rem; margin-bottom: 1rem">
-                        <div class="class d-flex flex-column align-items-start" style="margin: 0rem 1rem">
+                        <div class="class d-flex flex-column align-items-start justify-content-center" style="margin: 0rem 1rem">
                             <h4 >{{ $data->sectionName }}</h4>
                             <div class="d-flex">
                                 <p>Jumlah Ticket:</p>
                                 <p style="margin-left: 2rem">{{ $data->quantity }}</p>
                             </div>
                         </div>
-                        <div class="d-flex flex-column align-items-end" style="margin: 0rem 1rem">
-                            <p style="color: rgb(85, 83, 83)">Total</p>
-                            <h3 style="font-weight: bold">@currency( $data->total_price )</h3>
-                        </div>
-                    </div>
-                @endforeach
+                        <div class="d-flex flex-column align-items-end justify-content-center" style="margin: 0rem 1rem">
+                            <p style="color: rgb(85, 83, 83); text-align: right">Total</p>
+                            <h3 style="min-width: 14rem; font-weight: bold; text-align: right">@currency( $data->total_price )</h3>
+          @endforeach
             </div>
         </div>
     </div>
